2f643fd37a4e6c41496631f82351e587fc97e90a
mubashrazhar07 <110892453+mubashrazhar07@users.noreply.github.com>
2026-01-15 11:21:45 +0000

    Add signing config for release (KTS)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,33 @@
+import java.util.Properties
+import java.io.FileInputStream
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
 
 android {
+      signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
     namespace = "com.example.my_flutter_upload_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
